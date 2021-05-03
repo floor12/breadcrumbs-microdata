beb62dce324f8fa6806a4a51e1747efe80e1cc71
floor12 <floor12@floor12.net>
2021-05-03 18:08:32 +0200

    First implementation.

--- src/Breadcrumbs.php
+++ src/Breadcrumbs.php
@@ -6,5 +6,55 @@ namespace floor12\Breadcrumbs;
 
 class Breadcrumbs
 {
+    const HTML_OL = '<ol itemscope itemtype="http://schema.org/BreadcrumbList" class="%s" id="%s">%s</ol>';
+    const HTML_LI = '<li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">%s</li>';
+    const HTML_A = '<a data-pjax="0" itemprop="item" href="%s">%s</a>';
+    const HTML_SPAN = '<span itemprop="name">%s</span>';
+    const HTML_META_POSITION = '<meta itemprop="position" content="%d">';
 
+    protected $cssClass = 'f12-breadcrumbs';
+    protected $mainId = 'f12-breadcrumbs';
+    protected $elements = [];
+
+    public function addElement(string $name, string $url = null): self
+    {
+        $this->elements[$url] = $name;
+        return $this;
+    }
+
+    public function getHtml(): ?string
+    {
+        if (\count($this->elements) === 0) {
+            return null;
+        }
+        $renderedElements = [];
+        $position = 0;
+        foreach ($this->elements as $url => $name) {
+            $position++;
+            $element = $element = sprintf(self::HTML_SPAN, $name) . sprintf(self::HTML_META_POSITION, $position);
+            if (!is_numeric($url)) {
+                $element = sprintf(self::HTML_A, $url, $element);
+            }
+            $renderedElements[] = sprintf(self::HTML_LI, $element);
+        }
+
+        return sprintf(
+            self::HTML_OL,
+            $this->cssClass,
+            $this->mainId,
+            implode($renderedElements)
+        );
+    }
+
+    public function setCssClass(string $cssClass): self
+    {
+        $this->cssClass = $cssClass;
+        return $this;
+    }
+
+    public function setMainId(string $mainId): self
+    {
+        $this->mainId = $mainId;
+        return $this;
+    }
 }
--- tests/BreadcrumbTest.php
+++ tests/BreadcrumbTest.php
@@ -1,12 +1,56 @@
 <?php
 
+use floor12\Breadcrumbs\Breadcrumbs;
 use PHPUnit\Framework\TestCase;
 
 class BreadcrumbTest extends TestCase
 {
     public function testEmpty()
     {
-        $this->assertTrue(true);
+        $breadcrumbs = new Breadcrumbs();
+        $this->assertNull($breadcrumbs->getHtml());
+    }
+
+    public function testDefaultCss()
+    {
+        $breadcrumbs = new Breadcrumbs();
+        $breadcrumbs->addElement('Test element');
+        $this->assertStringContainsString('class="f12-breadcrumbs"', $breadcrumbs->getHtml());
+    }
+
+    public function testDefaultId()
+    {
+        $breadcrumbs = new Breadcrumbs();
+        $breadcrumbs->addElement('Test element');
+        $this->assertStringContainsString('id="f12-breadcrumbs"', $breadcrumbs->getHtml());
+    }
+
+    public function testCustomCss()
+    {
+        $breadcrumbs = new Breadcrumbs();
+        $breadcrumbs->addElement('Test element');
+        $breadcrumbs->setCssClass('test');
+        $this->assertStringContainsString('class="test"', $breadcrumbs->getHtml());
+    }
+
+    public function testCustomId()
+    {
+        $breadcrumbs = new Breadcrumbs();
+        $breadcrumbs->addElement('Test element');
+        $breadcrumbs->setMainId('test');
+        $this->assertStringContainsString('id="test"', $breadcrumbs->getHtml());
+    }
+
+    public function testSuccess()
+    {
+        $breadcrumbs = new Breadcrumbs();
+        $breadcrumbs->addElement('First element 1', '/first');
+        $breadcrumbs->addElement('Second element 2', '/first/second');
+        $breadcrumbs->addElement('Current');
+        $expectedResult = file_get_contents(__DIR__ . '/_result.html');
+        $expectedResult = preg_replace("/\r|\n|  /", "", $expectedResult);
+        $this->assertEquals($expectedResult, $breadcrumbs->getHtml());
     }
 
 }
+
